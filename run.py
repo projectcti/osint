a51e552b288d3613c714d8afa7dba91d366d28d6
leecybersec <leecybersec@gmail.com>
2020-07-13 23:44:43 +0700

    add ssl

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from app import create_app
+import ssl
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.verify_mode = ssl.CERT_REQUIRED
+context.load_verify_locations(".ssl/projectcti_com.ca-bundle")
+context.load_cert_chain(".ssl/projectcti_com.crt", ".ssl/key.pem")
 
 if __name__ == "__main__":
 	app = create_app('settings')
 	# app.run(host="0.0.0.0", port=5000, debug=True)
-	app.run(host="0.0.0.0", port=5000)
+	app.run(host="0.0.0.0", port=5000, ssl_context=context)
